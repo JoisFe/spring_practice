cffde0079f546bed8aede3cb4c098dbc3289ce3e
조재철 <rhsnqk@gmail.com>
2022-11-02 21:32:20 +0900

    [Feat]: taco design form 컨틀로러 테스트 코드 작성

---
+++ src/test/java/com/example/TacoCloud/controller/DesignTacoControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.TacoCloud.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
+@WebMvcTest
+class DesignTacoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void showDesignForm() throws Exception {
+        mockMvc.perform(get("/design"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("design"));
+    }
+}
