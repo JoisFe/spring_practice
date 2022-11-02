d34c8f08a0be2e7dc601d9f224872e24f92f46aa
조재철 <rhsnqk@gmail.com>
2022-11-02 21:20:19 +0900

    [Feat]: 타코 디자인 view 작성

--- src/main/java/com/example/TacoCloud/domain/Taco.java
+++ src/main/java/com/example/TacoCloud/domain/Taco.java
@@ -1,11 +1,13 @@
 package com.example.TacoCloud.domain;
 
 import java.util.List;
+import lombok.Getter;
 
 /**
  * @author : 조재철
  * @since 1.0
  */
+@Getter
 public class Taco {
     private String name;
     private List<String> ingredients;
