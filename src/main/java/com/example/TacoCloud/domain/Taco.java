a8ba570e8f7d1c0cb4e531b4ffcb3d7d2cb4e508
조재철 <rhsnqk@gmail.com>
2022-11-02 20:03:12 +0900

    taco domain 작성

---
+++ src/main/java/com/example/TacoCloud/domain/Taco.java
@@ -0,0 +1,12 @@
+package com.example.TacoCloud.domain;
+
+import java.util.List;
+
+/**
+ * @author : 조재철
+ * @since 1.0
+ */
+public class Taco {
+    private String name;
+    private List<String> ingredients;
+}
